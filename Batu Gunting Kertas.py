c8a466dc551a4ac3d51daa5f9b4529f7bf89b6a1
Kevin Dzaky <115269741+KeveenD@users.noreply.github.com>
2023-09-09 18:42:11 +0700

    Add files via upload

---
+++ Batu Gunting Kertas.py
@@ -0,0 +1,33 @@
+import random
+
+def get_choices():
+  player_choice = input("Masukkan pilihanmu (batu, kertas, gunting): ")
+  options = ["batu", "kertas", "gunting"]
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  return choices
+
+
+def check_win(player, computer):
+  print (f"Kamu memilih {player}, Komputer memilih {computer}")
+  if player == computer:
+    return "Imbang coy!"
+  elif player == "batu":
+    if computer == "gunting":
+      return "Batu menghancurkan gunting, kamu menang!"
+    else:
+      return "Batu dilapisi kertas, kamu kalah!"
+  elif player == "kertas":
+    if computer == "batu":
+      return "Kertas melapisi batu, kamu menang!"
+    else:
+      return "Kertas dipotong gunting, kamu kalah!"
+  elif player == "gunting":
+    if computer == "kertas":
+      return "Gunting memotong kertas, kamu menang!"
+    else:
+      return "Gunting dihancurkan batu, kamu kalah!"
+  
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
