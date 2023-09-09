30cb66c8f9498809b9f232bc6f9e5ed6bc9a3c7d
Kevin Dzaky <115269741+kevidn@users.noreply.github.com>
2023-09-09 22:20:28 +0700

    Batu Gunting Kertas.py

--- Batu Gunting Kertas.py
+++ Batu Gunting Kertas.py
@@ -1,6 +1,6 @@
 import random
 
-# Menerima Pilihan dari User Antara Batu, Kertas, Gunting
+# Menerima Pilihan dari User Batu, Kertas, atau Gunting
 def get_choices():
   player_choice = input("Masukkan pilihanmu (batu, kertas, gunting): ")
   options = ["batu", "kertas", "gunting"]
@@ -8,11 +8,15 @@ def get_choices():
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
 
-
+# Hasil dari pilihan yang random dari User dan Komputer
 def check_win(player, computer):
   print (f"Kamu memilih {player}, Komputer memilih {computer}")
+
+# Permainan imbang jika variabel sama antara User dan Komputer
   if player == computer:
     return "Imbang coy!"
+
+# Hasil Permainan jika variabel berbeda antara User dan Komputer
   elif player == "batu":
     if computer == "gunting":
       return "Batu menghancurkan gunting, kamu menang!"
